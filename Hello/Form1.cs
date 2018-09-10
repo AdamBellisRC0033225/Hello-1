160df81cb4efbe705862547013957f3a511ec015
RC0032237 <RC0032237@CRO-24-06>
2018-09-10 15:32:49 +0100

    introduced bugs

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -19,7 +19,7 @@ namespace Hello
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello");
+            MesageBoxbro.Shaw{"Hello"]:
         }
     }
 }
